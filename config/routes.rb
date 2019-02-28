684960e240f7c4955c70a9ed6b3f0426e98e9ca1
Michael Moffitt <mikeymoff91@gmail.com>
2019-02-27 19:07:52 -0700

    added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'subs#index'
+
+  # get 'subs/index' does the same as the item above
+
+  # resources :subs
+#the above line will create all of the CRUD actions
+  # get '/subs', to: 'subs#index'
+  # get '/subs/:id', to: 'subs#show'
+  # get '/subs/:id', to: 'subs#edit'
+  # put '/subs/:id', to: 'subs#update'
+  # delete '/subs/:id', to: 'subs#destroy'
+
+  resources :subs do
+    resources :topics
+
+  end
+
+
+
 end
